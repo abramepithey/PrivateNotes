5ba56930346e9d8ef79f32be2ebb9b0988c100ac
Abram Pithey <abramepithey@gmail.com>
2020-11-02 22:21:21 -0700

    Adds NoteController.cs, with an injected ApplicationDbContext, and an index and current user method

---
+++ PrivateNotes.MVC/Controllers/NoteController.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using PrivateNotes.Data;
+using PrivateNotes.Services;
+
+namespace PrivateNotes.MVC.Controllers
+{
+    public class NoteController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public NoteController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        
+        // GET
+        [Authorize]
+        public IActionResult Index()
+        {
+            var service = CreateNoteService();
+            var query = service.GetAllNotes();
+            return View(query);
+        }
+
+        private NoteService CreateNoteService()
+        {
+            var currentUser = this.User;
+            var id = currentUser.FindFirst(ClaimTypes.NameIdentifier).Value;
+            NoteService service = new NoteService(_context, Guid.Parse(id));
+            return service;
+        }
+    }
+}
